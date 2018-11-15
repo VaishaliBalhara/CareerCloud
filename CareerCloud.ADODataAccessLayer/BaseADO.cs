fb38601f2d70136b88bb5b6e44ec0fb84e69a1dd
Vaishali <vaishali_leo@rediffmail.com>
2018-11-15 04:03:52 -0500

    change1

---
+++ CareerCloud.ADODataAccessLayer/BaseADO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerCloud.ADODataAccessLayer
+{
+    public abstract class BaseADO
+    {
+        protected string connString;
+
+        public BaseADO()
+        {
+            connString = @"Data Source=.\HUMBERBRIDGING;Initial Catalog=JOB_PORTAL_DB;Integrated Security=True;";
+        }
+    }
+}
