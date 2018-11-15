fdf970019bd0c7a642dfe62fab89c9d74a196b32
Vaishali <vaishali_leo@rediffmail.com>
2018-11-15 07:31:48 -0500

    change1

---
+++ CareerCloud.ADODataAccessLayer/ApplicantJobApplicationRepository.cs
@@ -0,0 +1,76 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerCloud.ADODataAccessLayer
+{
+    class ApplicantJobApplicationRepository : BaseADO, IDataRepository<ApplicantJobApplicationPoco>
+    {
+        public void Add(params ApplicantJobApplicationPoco[] items)
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                SqlCommand command = new SqlCommand();
+                command.Connection = conn;
+
+            }
+        }
+
+        public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ApplicantJobApplicationPoco> GetAll(params System.Linq.Expressions.Expression<Func<ApplicantJobApplicationPoco, object>>[] navigationProperties)
+        {
+            ApplicantJobApplicationPoco[] pocos = new ApplicantJobApplicationPoco[500];
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                SqlCommand command = new SqlCommand("Select * from Applicant_Job_Applications", conn);
+                int position = 0;
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while(reader.Read())
+                {
+                    ApplicantJobApplicationPoco poco = new ApplicantJobApplicationPoco();
+                    poco.Id = reader.GetGuid(0);
+                    poco.Applicant = reader.GetGuid(1);
+                    poco.Job = reader.GetGuid(2);
+                    poco.ApplicationDate = reader.GetDateTime(3);
+                    poco.TimeStamp = (byte[])reader[4];
+
+                    pocos[position] = poco;
+                    position++;
+
+                }
+            }
+            return pocos.ToList();
+        }
+
+        public IList<ApplicantJobApplicationPoco> GetList(System.Linq.Expressions.Expression<Func<ApplicantJobApplicationPoco, bool>> where, params System.Linq.Expressions.Expression<Func<ApplicantJobApplicationPoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ApplicantJobApplicationPoco GetSingle(System.Linq.Expressions.Expression<Func<ApplicantJobApplicationPoco, bool>> where, params System.Linq.Expressions.Expression<Func<ApplicantJobApplicationPoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(params ApplicantJobApplicationPoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(params ApplicantJobApplicationPoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
