9fb7568af375615996f9ba084bc87870fca76511
Vaishali <vaishali_leo@rediffmail.com>
2019-01-07 16:22:10 -0500

    a4

---
+++ CareerCloud.BusinessLogicLayer/CompanyLocationLogic.cs
@@ -0,0 +1,66 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class CompanyLocationLogic:BaseLogic<CompanyLocationPoco>
+    {
+        public CompanyLocationLogic(IDataRepository<CompanyLocationPoco> repository) : base(repository)
+        {
+        }
+
+        public override void Add(CompanyLocationPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Add(pocos);
+        }
+
+        public override void Update(CompanyLocationPoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+
+        protected override void Verify(CompanyLocationPoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+            foreach (CompanyLocationPoco item in pocos)
+            {
+                if (item.CountryCode.Length<0)
+                {
+                    exceptions.Add(new ValidationException(500, "Country code can not be empty"));
+                }
+
+                if (item.Province.Length<0)
+                {
+                    exceptions.Add(new ValidationException(501, "Province can not be empty"));
+                }
+
+                if (item.Street.Length < 0)
+                {
+                    exceptions.Add(new ValidationException(502, "Street can not be empty"));
+                }
+
+                if (item.City.Length < 0)
+                {
+                    exceptions.Add(new ValidationException(503, "City can not be empty"));
+                }
+
+                if (item.PostalCode.Length<0)
+                {
+                    exceptions.Add(new ValidationException(504, "Postal code can not be empty"));
+                }
+
+                if (exceptions.Count > 0)
+                {
+                    throw new AggregateException(exceptions);
+                }
+            }
+        }
+    }
+}
---
+++ CareerCloud.BusinessLogicLayer/CompanyProfileLogic.cs
@@ -0,0 +1,51 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class CompanyProfileLogic:BaseLogic<CompanyProfilePoco>
+    {
+        public CompanyProfileLogic(IDataRepository<ApplicantResumePoco> repository) : base(repository)
+        {
+        }
+
+        public override void Add(CompanyProfilePoco[] pocos)
+        {
+            Verify(pocos);
+            base.Add(pocos);
+        }
+
+        public override void Update(CompanyProfilePoco[] pocos)
+        {
+            Verify(pocos);
+            base.Update(pocos);
+        }
+
+        protected override void Verify(CompanyProfilePoco[] pocos)
+        {
+            List<ValidationException> exceptions = new List<ValidationException>();
+            foreach (CompanyProfilePoco item in pocos)
+            {
+                if (true)
+                {
+                    exceptions.Add(new ValidationException(600, "Website must end with '.ca','.com' or '.biz'"));
+                }
+
+                if (true)
+                {
+                    exceptions.Add(new ValidationException(601, "Not a valid phone number"));
+                }
+
+                if (exceptions.Count > 0)
+                {
+                    throw new AggregateException(exceptions);
+                }
+            }
+        }
+    }
+}
